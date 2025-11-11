ec95bae508c7a1b7d0d90db5a994f39711136b5e
Mohamed-666creator <nodeysoso1@gmail.com>
2025-11-11 18:07:48 +0200

    Add files via upload

---
+++ src/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>Created by محمد أحمد خلف</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import About from './About.jsx';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Smart Farmer</h1>
+      <About />
+    </div>
+  );
+}
+
+export default App;
